946af0bb51b31091fc1a1e2bb09e8732c45c88e6
ElimuStuart <stuartelimu@gmail.com>
2020-06-04 10:22:49 +0300

    create view for questions

--- products/views.py
+++ products/views.py
@@ -1,8 +1,14 @@
 from django.views.generic import ListView
 
-from .models import Product
+from .models import Product, Question
 
 class ProductListView(ListView):
     model = Product
     context_object_name = 'products'
     template_name = 'products.html'
+
+
+class QuestionList(ListView):
+    model = Question
+    context_object_name = 'questions'
+    template_name = 'faq.html'
