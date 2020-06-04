b746c02e458060ce36816a9675e70491c32bcf00
ElimuStuart <stuartelimu@gmail.com>
2020-06-04 11:14:52 +0300

    add page restrictions

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 from .models import Product, Question
 
@@ -8,6 +10,7 @@ class ProductListView(ListView):
     template_name = 'products.html'
 
 
+@method_decorator(login_required, name='dispatch')
 class QuestionList(ListView):
     model = Question
     context_object_name = 'questions'
--- src/views.py
+++ src/views.py
@@ -1,8 +1,12 @@
 from django.views.generic import View
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 from products.models import Notification, Question
 
 
+@method_decorator(login_required, name='dispatch')
 class Index(View):
     template_name = 'index.html'
 
