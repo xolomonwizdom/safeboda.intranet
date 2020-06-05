c4772054563f0cb8eee27fe2b7bed6ec70c67dd2
ElimuStuart <stuartelimu@gmail.com>
2020-06-05 06:45:49 +0300

    increase number of questions

--- src/views.py
+++ src/views.py
@@ -12,5 +12,5 @@ class Index(View):
 
     def get(self, request, *args, **kwargs):
         recent_notifications = Notification.objects.all()[1:4]
-        featured_qns = Question.objects.filter(featured=True)[:3]
+        featured_qns = Question.objects.filter(featured=True)[:5]
         return render(request, self.template_name, {'notifications': recent_notifications, 'questions': featured_qns})
