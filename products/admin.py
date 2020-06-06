1b5841aaaee4787dd97fa65d797ca75b646b8a53
ElimuStuart <stuartelimu@gmail.com>
2020-06-06 20:49:15 +0300

    add tools to admin, create tooladmin

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from products.models import Product, Activity, Question, Notification
+from products.models import ProductType, Scenario, Question, Notification, Tool
 
-admin.site.register(Product)
-admin.site.register(Activity)
+admin.site.register(ProductType)
+admin.site.register(Scenario)
 admin.site.register(Question)
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
     list_display = ['title', 'resolved', 'updated_at']
+
+
+@admin.register(Tool)
+class ToolAdmin(admin.ModelAdmin):
+    list_display = ['name', 'link']
